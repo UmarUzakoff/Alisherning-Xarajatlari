42fc0fbf52596ca51e8d7decee0931545425eff3
UzakoffUmar <uzakovumar338@gmail.com>
2022-12-04 14:50:47 +0500

    travel

---
+++ index.js
@@ -0,0 +1,9 @@
+var LOL = prompt("Whats is your name bro?");
+var really = prompt(`So, you want to travel around the world ${LOL}?`);
+var money = prompt(`${LOL}, How much money do you have my brother?`);
+
+if (money > 1000000) {
+    alert(`${LOL}, Good Luck then!`)
+} else {
+    alert(`${LOL}, You need to be patient, keep going!)`)
+}
